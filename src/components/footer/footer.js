c4502cca6dad80e168d07b23cc60bea89814c7f0
samleivadev <samdleiva@gmail.com>
2023-04-05 00:41:36 -0400

    Basic header and footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import style from '../footer/footer.module.css'
 
 const Footer = () => {
     return(
-        <div>
-            <h2>Work hard, have fun, make history</h2>
-        </div>
+        <section>
+            <hr />
+            <section className={style.topfooter}>
+              <h2>Work hard, have fun, make history</h2>  
+            </section>
+            <section className={style.finfo}>
+                <section className={style.fleft}>
+                    Meta Engineers
+                </section>
+                <section className={style.fcenter}>
+                    slack @calsamue
+                </section>
+                <section className={style.fright}>
+                    CVG2
+                </section>
+            </section>
+            
+        </section>
     );
 }
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,15 @@
+import { NavLink } from 'react-router-dom';
 import React from 'react';
+import style from '../header/header.module.css';
 
 const Header = () => {
     return(
-        <div>
+        <div className={style.header}>
             <h2>Control Tower</h2>
+            <ul>
+                <NavLink className={style.menu} to='/'>Home</NavLink>
+                <NavLink className={style.menu} to='/kiosk'>Kiosk</NavLink>
+            </ul>
         </div>
     );
 }
