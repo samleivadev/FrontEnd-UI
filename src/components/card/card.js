c7fbe71ada219540972e68924c306dbc673b4852
samleivadev <samdleiva@gmail.com>
2023-04-06 23:18:51 -0400

    Create cards for Employee Info

---
+++ src/components/card/card.js
@@ -0,0 +1,44 @@
+import React from "react";
+import style from "../card/card.module.css"
+
+function AACard(props){
+    const {aaName, imageAA, aaDep, aaLogin, aaID, aaShift,amName, imageAM, amTitle, amID} = props
+  
+    let amPic = `url(${props.imageAM})`
+    let aaPic = `url(${props.imageAA})`
+  return(
+    <div className={style.bioCard}>
+        <div className={style.upperContainer}>
+            <div className={style.imgContainer}>
+            <img src={props.imageAA} alt="" height='150px' width="150px" />
+            
+            </div>
+        </div>
+        <div className={style.lowerContainer}>
+            <div className={style.devName}>{aaName}</div>
+            <div className={style.devTitle}>{aaLogin}</div>
+            <div className={style.devDesc}>{aaID}</div>
+            <div className={style.devDesc}>{aaShift}</div>
+            <div className={style.devDesc}>{aaDep}</div>
+                            
+        </div>
+        
+        <div className={style.upperContainer}>
+            <div className={style.imgContainer}>
+            <img src={props.imageAM} alt="" height='100px' width="100px" />
+            
+        </div>
+        <div className={style.lowerContainer}>
+            <div className={style.devName}>{amName}</div>
+            <div className={style.devTitle}>{amTitle}</div>
+            <div className={style.devTitle}>{amID}</div>
+           
+                        
+        </div>
+                
+        </div>
+    </div>
+  )  
+}
+
+export default AACard;
--- src/pages/kiosk/kiosk.js
+++ src/pages/kiosk/kiosk.js
@@ -1,9 +1,25 @@
+import AACard from '../../components/card/card.js';
 import React from 'react';
+import style from '../kiosk/kiosk.module.css'
 
 const KioskPage = () => {
     return(
         <div>
-            <h2>Welcome to the Kiosk Page</h2>
+            
+             <AACard
+                
+                aaName="Samuel Leiva"
+                imageAA ='https://media.licdn.com/dms/image/D5635AQHyIoo2Ijofzw/profile-framedphoto-shrink_200_200/0/1658445629590?e=1681444800&v=beta&t=e56A3kM2_UqQ04m4fj00D0Q0ja_Ezl55tilR8ikXM0E'
+                aaLogin="Process Assistant"
+                aaID="3336339"
+                aaShift="DB-715"
+                aaDep="Kindle"
+                imageAM='https://media.licdn.com/dms/image/C4D03AQFjZlCOqmJUNw/profile-displayphoto-shrink_200_200/0/1517373325396?e=1686182400&v=beta&t=TOO_wNgb_Tgd4zbfRpr-jM9qy7HaKrMZ7E6n42Th4cM'
+                amName='Craig Stearns'
+                amTitle='Area Manager'
+                amID='mmstris'
+              
+            />
         </div>
     );
 }
