fac477033c4f99938530f47159902bddc4b65885
samleivadev <samdleiva@gmail.com>
2023-04-07 00:07:31 -0400

    Add Logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,3 +1,4 @@
+import Logo from '../../assets/metaCT-Logo.png'
 import { NavLink } from 'react-router-dom';
 import React from 'react';
 import style from '../header/header.module.css';
@@ -5,7 +6,9 @@ import style from '../header/header.module.css';
 const Header = () => {
     return(
         <div className={style.header}>
-            <div className={style.logo}>Control Tower</div>
+            <div className={style.logo}>
+                <img src={Logo} alt="bug" height={100} />
+            </div>
             <ul className={style.menus}>
                 <NavLink className={style.menu} to='/'>Home</NavLink>
                 <NavLink className={style.menu} to='/kiosk'>Kiosk</NavLink>
