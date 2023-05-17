2d5d083912716e133b25ff2e10d319553313b58e
samleivadev <samdleiva@gmail.com>
2023-05-17 00:32:19 -0400

    Home page update

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import  '../home/home.css'
 
-const HomePage = () => {
-    return(
-        <div>
-            <h2>Welcome to the home page</h2>
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+function HomePage () {
+    const [employeeId, setEmployeeId] = useState('');
+    const [employeeData, setEmployeeData] = useState(null);
+  
+    const handleInputChange = event => {
+      setEmployeeId(event.target.value);
+    };
+  
+    const handleSubmit = async event => {
+      event.preventDefault();
+      try {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${employeeId}`);
+        setEmployeeData(response.data);
+      } catch (error) {
+        console.error(error);
+        setEmployeeData(null);
+      }
+    };
+  
+    useEffect(() => {
+      const handleKeyPress = event => {
+        if (event.key === 'Enter') {
+          handleSubmit(event);
+        }
+      };
+  
+      document.addEventListener('keypress', handleKeyPress);
+      return () => {
+        document.removeEventListener('keypress', handleKeyPress);
+      };
+    }, [handleSubmit]);
+  
+    return (
+        <div className="app-container">
+          <div className="form-container">
+            <h1>Welcome</h1>
+            <form onSubmit={handleSubmit}>
+              <input type="text" value={employeeId} onChange={handleInputChange} placeholder="Scan your Badge here" />
+            </form>
+            {employeeData && (
+              <div>
+                <p>Badge ID: {employeeData.id}</p>
+                <p>Name: {employeeData.name}</p>
+                <p>Manager: {employeeData.email}</p>
+              </div>
+            )}
+          </div>
+          <div className="employee-details-container">
+            {employeeData && (
+              <div>
+                <h2>Today you are goin to work on:</h2>
+                <p>Workstation: {employeeData.phone}</p>
+                <p>PA Assigned: {employeeData.website}</p>
+              </div>
+            )}
+          </div>
         </div>
-    );
-}
+      );
+      
+  }
 
 export default HomePage;
