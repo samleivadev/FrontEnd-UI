4fe1f853d16229534af9de74f49125998c37c595
samleivadev <samdleiva@gmail.com>
2023-05-17 01:10:23 -0400

    clean information and clean form

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -13,15 +13,21 @@ function HomePage () {
     };
   
     const handleSubmit = async event => {
-      event.preventDefault();
-      try {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${employeeId}`);
-        setEmployeeData(response.data);
-      } catch (error) {
-        console.error(error);
-        setEmployeeData(null);
-      }
-    };
+        event.preventDefault();
+        try {
+          const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${employeeId}`);
+          setEmployeeData(response.data);
+          setTimeout(() => {
+            setEmployeeData(null);
+            setEmployeeId(''); // Clear the form input
+          }, 5000); // Hide employee info after 5 seconds (5000 milliseconds)
+        } catch (error) {
+          console.error(error);
+          setEmployeeData(null);
+          setEmployeeId(''); // Clear the form input
+        }
+      };
+      
   
     useEffect(() => {
       const handleKeyPress = event => {
@@ -37,32 +43,31 @@ function HomePage () {
     }, [handleSubmit]);
   
     return (
-        <div className="app-container">
-          <div className="form-container">
-            <h1>Welcome</h1>
-            <form onSubmit={handleSubmit}>
-              <input type="text" value={employeeId} onChange={handleInputChange} placeholder="Scan your Badge here" />
-            </form>
-            {employeeData && (
-              <div>
-                <p>Badge ID: {employeeData.id}</p>
-                <p>Name: {employeeData.name}</p>
-                <p>Manager: {employeeData.email}</p>
-              </div>
-            )}
-          </div>
-          <div className="employee-details-container">
-            {employeeData && (
-              <div>
-                <h2>Today you are goin to work on:</h2>
-                <p>Workstation: {employeeData.phone}</p>
-                <p>PA Assigned: {employeeData.website}</p>
-              </div>
-            )}
-          </div>
+      <div className="app-container">
+        <div className="form-container">
+          <h1>Welcome</h1>
+          <form onSubmit={handleSubmit}>
+            <input type="text" value={employeeId} onChange={handleInputChange} placeholder="Scan Your badge here" />
+          </form>
+          {employeeData && (
+            <div>
+              <p>Badge ID: {employeeData.id}</p>
+              <p>Name: {employeeData.name}</p>
+              <p>Manager: {employeeData.email}</p>
+            </div>
+          )}
         </div>
-      );
-      
+        <div className="employee-details-container">
+          {employeeData && (
+            <div>
+              <h2>Today you are going to work in</h2>
+              <p>Worstation: {employeeData.phone}</p>
+              <p>PA Assigned: {employeeData.website}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
   }
 
 export default HomePage;
